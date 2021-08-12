0a080480324255e7b3acf5555252edf8a1ef3a2c
itscal123 <calvin.kory@gmail.com>
2021-08-11 21:58:08 -0700

    Update preprocess_data.py

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,7 +3,14 @@ from pathlib import Path
 import pandas as pd
 
 
-df = pd.read_csv("data/data.csv")
-
-df = pd.DataFrame(MinMaxScaler().fit_transform(df))
-print(pd.head())
+def preprocess():
+    """
+    Reads the data.csv in the data folder, drops the timestamp column (1st column),
+    normalizes the data, and returns the mutated data.
+    params: None
+    returns: numpy.ndarray (n_samples, n_features_new)
+    """
+    df = pd.read_csv("data/data.csv")
+    df = df.iloc[:,1:]
+    data = MinMaxScaler().fit_transform(df)
+    return data
