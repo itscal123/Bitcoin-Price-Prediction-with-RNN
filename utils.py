8969cedbaaad600fc22f27714e0d8d23be338186
itscal123 <calvin.kory@gmail.com>
2021-08-12 14:41:38 -0700

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,12 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_multiple_forecasts(X, Y, Y_pred):
+    n_steps = X.shape[1]
+    ahead = Y.shape[1]
+    plot_series(X[0, :, 0])
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y[0, :, 0], "ro-", label="Actual")
+    plt.plot(np.arange(n_steps, n_steps + ahead), Y_pred[0, :, 0], "bx-", label="Forecast", markersize=10)
+    plt.axis([0, n_steps + ahead, -1, 1])
+    plt.legend(fontsize=14)
