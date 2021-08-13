f0b2babfae0f2b6c0aa0eb14c2afd3b012b635f4
itscal123 <calvin.kory@gmail.com>
2021-08-12 21:08:40 -0700

    Update train_RNN.py

--- train_RNN.py
+++ train_RNN.py
@@ -11,9 +11,10 @@ def last_time_step_mse(Y_true, Y_pred):
 if __name__ == "__main__":
     train, test = get_data()
     model = keras.models.Sequential([
-        keras.layers.LSTM(20, return_sequences=True, input_shape=[3,16]),
+        keras.layers.LSTM(20, return_sequences=True),
         keras.layers.LSTM(20, return_sequences=True),
         keras.layers.TimeDistributed(keras.layers.Dense(3))
     ])
-    model.compile(loss="mean_squared_logarithmic_error", optimizer="nadam")
-    history = model.fit(train, epochs=20, batch_size=3)
+    model.compile(loss="mae", optimizer="nadam")
+    history = model.fit(train, epochs=10)
+    model.save("saved_model\my_model")  
